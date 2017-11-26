0be3bb3dfe5161b3327c7973f19db5c15483c2e8
Isaias Morochi <isaiasmorochi95@gmail.com>
2017-11-26 00:39:31 -0400

    Actualizado

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/AdmGeneral/GestionarInstitucion';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new controller instance.
